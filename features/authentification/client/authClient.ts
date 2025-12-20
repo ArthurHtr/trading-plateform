5eaf4aad11c0e9643c81fdae1e45bff6ed79a853
Arthur Hottier <arthur.ho2tier@gmail.com>
2025-12-20 14:28:10 +0100

    vire base url code en dur dans authclient

--- features/authentification/client/authClient.ts
+++ features/authentification/client/authClient.ts
@@ -5,7 +5,6 @@ import { apiKeyClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
   plugins: [
     apiKeyClient()
   ]

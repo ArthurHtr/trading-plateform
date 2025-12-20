262fa84c516b392e2fb7d5714f8ca63872dda9bc
Arthur Hottier <arthur.ho2tier@gmail.com>
2025-12-20 11:24:15 +0100

    fichier prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,8 +3,14 @@ import { PrismaClient } from "@prisma/client"
 import dotenv from "dotenv"
 import path from "path"
 
-if (!process.env.DATABASE_URL) {
-  dotenv.config({ path: path.resolve(process.cwd(), '.env') })
+if (process.env.NODE_ENV === "production") {
+  // En PROD : Charge le .env depuis un chemin spécifique
+  dotenv.config({ path: path.resolve(process.cwd(), "/etc/tbf_tpf_prod/tbf_tpf_prod.env") })
+} else {
+  // En DEV : Charge le .env depuis la racine si nécessaire
+  if (!process.env.DATABASE_URL) {
+    dotenv.config({ path: path.resolve(process.cwd(), ".env") })
+  }
 }
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
